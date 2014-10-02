d2f8362d5b123efd6f90e4a71b11eb937c1d40f3
jonschlinkert <dev@sellside.com>
2014-10-02 03:18:09 -0400

    v0.1.1

--- index.js
+++ index.js
@@ -7,30 +7,30 @@
 
 'use strict';
 
-module.exports = function shuffle(str, letters) {
+module.exports = function(str, letters) {
   var words = str.split(' ');
+
+  // see: http://stackoverflow.com/a/6274398/1267639
+  function shuffle(arr) {
+    var len = arr.length;
+    var swap;
+    var i;
+
+    while (len > 0) {
+      i = Math.floor(Math.random() * len);
+      len--;
+      swap = arr[len];
+      arr[len] = arr[i];
+      arr[i] = swap;
+    }
+    return arr;
+  }
 
   if (letters) {
     words = words.map(function(word) {
       return shuffle(word.split('')).join('');
     });
   }
-
   return shuffle(words).join(' ');
 };
 
-// see: http://stackoverflow.com/a/6274398/1267639
-function shuffle(arr) {
-  var len = arr.length;
-  var swap;
-  var i;
-
-  while (len > 0) {
-    i = Math.floor(Math.random() * len);
-    len--;
-    swap = arr[len];
-    arr[len] = arr[i];
-    arr[i] = swap;
-  }
-  return arr;
-}
